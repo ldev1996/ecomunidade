c1b85a8152e17d6051f8fbe71e3839367a1d7ba3
ldev1996 <leonardo.dev1996@gmail.com>
2025-05-08 15:23:22 -0300

    supabse db + tailwind css setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxt/icon', '@nuxtjs/tailwindcss']
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: true },
+    css: ['@/assets/css/main.css'],
+    modules: [
+        '@nuxt/icon',
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/supabase'
+    ],
+    supabase: {
+        redirectOptions: {
+            login: '/login',
+            callback: '/confirm',
+            include: undefined,
+            exclude: ['/', '/register'],
+            saveRedirectToCookie: true,
+        },
+        url: process.env.SUPABASE_URL,
+        key: process.env.SUPABASE_ANON_KEY
+    }
 })
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <p class="text-3xl text-center">Hello World!</p>
+</template>
+
+<script lang='js'>
+
+</script>
