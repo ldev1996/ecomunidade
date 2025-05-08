efb9d705bb22d012150bfa9171b37991c168407d
ldev1996 <leonardo.dev1996@gmail.com>
2025-05-08 19:35:05 -0300

    auth layout base

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,3 +1,7 @@
 <template>
-    <slot></slot>
+    <div class="min-h-screen flex flex-col items-center justify-center px-6">
+        <main class="w-100 max-w-[90vw] m-2 rounded-2xl bg-main-100 dark:bg-main-900 shadow-xl aspect-[9/13]">
+            <slot></slot>
+        </main>        
+    </div>
 </template>
