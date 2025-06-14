9fd3f5ee4f95b17ed6f2bb0bc59a4b709c70ebbe
ldev1996 <leonardo.dev1996@gmail.com>
2025-06-14 14:56:12 -0300

    feature: logo component

---
+++ components/Logo.vue
@@ -0,0 +1,29 @@
+<template>
+    <div :class="['flex flex-row items-center space-2', size, ui]">
+        <img
+            src="/icon.svg"
+            alt="Logo da ECOmunidade"
+            class="w-auto h-[1.4em]"
+        />
+        <span class="text-primary">
+            {{ name }}
+        </span>
+    </div>
+</template>
+
+<script setup lang="js">
+    const props = defineProps({
+        name: {
+            type: String,
+            default: 'ECOmunidade'
+        },
+        size: {
+            type: String,
+            default: 'text-2xl'
+        },
+        ui: {
+            type: String,
+            default: ''
+        }
+    })
+</script>
