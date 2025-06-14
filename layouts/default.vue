f96d0c51da916a922e5395b5c123adb0701ac60b
ldev1996 <leonardo.dev1996@gmail.com>
2025-06-14 14:57:00 -0300

    feature: default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,46 @@
 <template>
-    <slot />
+    <UContainer
+        as="header"
+        class="border-b border-neutral-200 dark:border-neutral-700 flex flex-row items-center justify-between py-2"
+    >
+        <div class="flex flex-row items-center gap-4">
+            <Logo />
+            <UNavigationMenu
+                v-if="!isMobile"
+                :items="navItems"
+                variant="link"
+                color="primary"
+            />
+        </div>
+    </UContainer>
+    <UContainer as="main" class="mt-2">
+        <slot />
+    </UContainer>
+    <UContainer
+        v-if="isMobile"
+        as="footer"
+        class="fixed left-0 bottom-0 z-10 border-t border-neutral-200 dark:border-neutral-700 flex flex-row items-center justify-center py-2"
+    >
+        <UNavigationMenu
+            :items="navItems"
+            variant="link"
+            color="primary"
+            labelKey=""
+        />
+    </UContainer>
 </template>
 
-<script setup lang="js"></script>
+<script setup lang="js">
+    import { useBreakpoints, breakpointsTailwind } from '@vueuse/core'
+
+    const breakpoints = useBreakpoints(breakpointsTailwind)
+    const isMobile = breakpoints.smallerOrEqual('sm')
+
+    const navItems = [
+        {
+            label: 'Home',
+            icon: 'lucide:house',
+            to: '/'
+        }
+    ]
+</script>
